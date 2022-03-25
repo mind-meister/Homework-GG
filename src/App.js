d0452e63e42193e356fd586ac3196ca6f42e3522
charlesprabowo <charles_prabowo@yahoo.co.id>
2022-03-25 16:46:44 +0700

    Homework Module2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Session2 from './Homework-Module-2/Session2';
+// import Session1 from './Homework-Module-2/Session1';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {/* <Session1 /> */}
+     <Session2 />
     </div>
   );
 }
---
+++ src/Homework-Module-2/Session2/componentImage.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+function Images(props){
+    return (
+        <div>
+            <img className="image" alt="avatar" src={props.src} />
+        </div>
+    );
+}
+
+export default Images
